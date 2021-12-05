6ea2f155eba4526d70156c5e123b2ab40d8f4ca3
Alexander Ryakhin <a.ryahin@trinitydigital.ru>
2021-12-05 20:26:42 +0300

    initial commit

--- population/main.c
+++ population/main.c
@@ -6,9 +6,40 @@
 //
 
 #include <stdio.h>
+#include "cs50.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(void)
+{
+    // Prompt for start size
+    int startSize;
+    do
+    {
+        startSize = get_int("Start size: ");
+    }
+    while (startSize < 9);
+
+    // Prompt for end size
+    int endSize;
+    do
+    {
+        endSize = get_int("End size: ");
+    }
+    while (endSize < startSize);
+
+    // Calculate number of years until we reach threshold
+    int counter = 0;
+    if (startSize != endSize)
+    {
+        do
+        {
+            int increase = startSize / 3;
+            int decrease = startSize / 4;
+            startSize = startSize + increase - decrease;
+            counter += 1;
+        }
+        while (startSize < endSize);
+    }
+
+    // Print number of years
+    printf("Years: %i\n", counter);
 }
