7ad9b0037988c0a19a3d59e96f86fdb077cb5087
Alexander Ryakhin <a.ryahin@trinitydigital.ru>
2021-12-05 20:23:17 +0300

    Initial Commit

---
+++ population/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  population
+//
+//  Created by Alexander Ryakhin on 12/5/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
